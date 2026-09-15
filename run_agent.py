Carrie111998/Hermes

fix(codex): preserve live reasoning source identity

run_agent.py
@@ -493,6 +493,7 @@ class AIAgent:
         notice_clear_callback: callable = None,
         event_callback: Optional[Callable[[str, dict], None]] = None,
         reaction_callback: Optional[Callable[[str], None]] = None,
+        reasoning_event_callback: callable = None,
         max_tokens: int = None,
         reasoning_config: Dict[str, Any] = None,
         service_tier: str = None,
@@ -584,6 +585,7 @@ class AIAgent:
             notice_clear_callback=notice_clear_callback,
             event_callback=event_callback,
             reaction_callback=reaction_callback,
+            reasoning_event_callback=reasoning_event_callback,
             max_tokens=max_tokens,
             reasoning_config=reasoning_config,
             service_tier=service_tier,

royalaid/hermes-agent

run_agent.py
@@ -247,7 +247,8 @@ class AIAgent(
         session_id: str = None,
         tool_progress_callback: callable = None, tool_start_callback: callable = None,
         tool_complete_callback: callable = None, thinking_callback: callable = None,
-        reasoning_callback: callable = None, clarify_callback: callable = None,
+        reasoning_callback: callable = None, reasoning_event_callback: callable = None,
+        clarify_callback: callable = None,
         read_terminal_callback: callable = None, read_preview_callback: callable = None,
         drive_preview_callback: callable = None, read_window_below_callback: callable = None,
         connection_callback: callable = None, tour_callback: callable = None, step_callback: callable = None,